226a7f545a0c6c7f0a9c14901e87c5a9245345b4
SayyedAquib <sayyed.aquib89@gmail.com>
2025-06-09 20:10:23 +0530

    refactor: improve AddToCartBtn component by consolidating state selectors, enhancing item existence checks, and refining button styling for better readability and user experience

--- src/components/AddToCartBtn.jsx
+++ src/components/AddToCartBtn.jsx
@@ -1,41 +1,51 @@
-import { addToCart } from "../utils/cartSlice";
 import { useDispatch, useSelector } from "react-redux";
+import { addToCart } from "../utils/cartSlice";
 import toast from "react-hot-toast";
 
 const AddToCartBtn = ({ info, resInfo, handleIsDiffRes }) => {
-  const cartData = useSelector((state) => state.cartSlice.cartItems);
-  const getResInfoFromLocalStore = useSelector(
-    (state) => state.cartSlice.resInfo
-  );
   const dispatch = useDispatch();
 
-  const handleAddToCart = () => {
-    const isAdded = cartData.find((data) => data.id === info.id);
+  const cartItems = useSelector((state) => state.cartSlice.cartItems);
+  const currentResInfo = useSelector((state) => state.cartSlice.resInfo);
 
-    if (!isAdded) {
-      if (
-        getResInfoFromLocalStore.name === resInfo.name ||
-        getResInfoFromLocalStore.length === 0
-      ) {
-        dispatch(addToCart({ info, resInfo }));
-        toast.success("food added to the cart");
-      } else {
-        toast.error("different restaurant ");
-        handleIsDiffRes();
-      }
-    } else {
-      toast.error("Already added ");
+  const itemExists = cartItems.some((item) => item.id === info.id);
+  const isSameRestaurant =
+    currentResInfo.length === 0 || currentResInfo.name === resInfo.name;
+
+  const handleAddToCart = () => {
+    if (itemExists) {
+      toast.error("Item already in the cart");
+      return;
     }
-  }
+
+    if (isSameRestaurant) {
+      dispatch(addToCart({ info, resInfo }));
+      toast.success("Item added to the cart");
+    } else {
+      toast.error("Items from different restaurants cannot be added");
+      handleIsDiffRes();
+    }
+  };
+
+  const getButtonStyle = () => {
+    if (itemExists) {
+      return "bg-gray-200 text-gray-500 cursor-not-allowed";
+    }
+    if (!isSameRestaurant) {
+      return "bg-red-100 text-red-600 border-red-400 cursor-pointer";
+    }
+    return "bg-white text-green-700 border-green-500 hover:bg-green-50 cursor-pointer";
+  };
 
   return (
     <button
       onClick={handleAddToCart}
-      className="bg-white absolute bottom-[-20px] left-1/2  -translate-x-1/2 text-lg text-green-700 font-bold rounded-xl border px-10 py-2 drop-shadow"
+      className={`absolute bottom-[-20px] left-1/2 -translate-x-1/2 text-lg font-bold rounded-xl border px-10 py-2 drop-shadow transition-all duration-200 ${getButtonStyle()}`}
+      disabled={itemExists}
     >
-      Add
+      {itemExists ? "Added" : "Add"}
     </button>
   );
-}
+};
 
 export default AddToCartBtn;
