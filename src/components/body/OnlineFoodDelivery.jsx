f983e0075983242706750b31e00be23d502484c5
SayyedAquib <sayyed.aquib89@gmail.com>
2025-07-10 11:21:42 +0530

    correct css for chrome of online food delivery

--- src/components/body/OnlineFoodDelivery.jsx
+++ src/components/body/OnlineFoodDelivery.jsx
@@ -37,7 +37,7 @@ const OnlineFoodDelivery = ({ data, title }) => {
         ))}
       </div>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-10">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 3xl:grid-cols-4 gap-10">
         {data.map(({ info, cta: { link } }) => (
           <div className="hover:scale-95 duration-300" key={info.id}>
             <RestaurantCard {...info} link={link} />
